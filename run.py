f558e871e08be72fbc70bce9bce9f1d556f9ae8e
Edouard Mignan <edouard.mignan@epfl.ch>
2026-04-15 20:05:05 +0200

    add basic cli

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from src.fuzzer import SQLFuzzer
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="command to execute", choices=["fuzz", "test"])
+    parser.add_argument("--file", type=str, help="path to a file containing a single SQL query to test")
+    args = parser.parse_args()
+    
     fuzzer = SQLFuzzer()
-    fuzzer.run()
+    
+    if args.command == "fuzz":
+        fuzzer.run()
+    elif args.command == "test" and args.file:
+        with open(args.file, "r") as f:
+            query = "".join(f.read().splitlines())
+            result = fuzzer.executor.execute(query)
+            fuzzer.report_bug(result, query)
