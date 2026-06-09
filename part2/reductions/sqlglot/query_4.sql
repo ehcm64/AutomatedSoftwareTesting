5b2bff63fdfc60a92bc8ee67bd5f0225fa208576
eamihai <eduardmihai958@gmail.com>
2026-06-09 23:57:18 +0200

    Add dqlglot reductions

---
+++ part2/reductions/sqlglot/query_4.sql
@@ -0,0 +1,1 @@
+CREATE TABLE t1 (col0, col1, col2, col3, col4); INSERT INTO t1 (col0, col1, col2, col3, col4) VALUES (NULL, NULL, 'av', -1252356455, 'Yjca'); INSERT INTO t1 (col0, col1, col2, col4) VALUES (NULL, 2049300292, 'sYCWGtg8J', 'RxYwTBHZ3'); SELECT LAG(-(-(-403961669))) OVER (), AVG(UPPER((col2 || 'HAFG'))) OVER (PARTITION BY col4), ROW_NUMBER() OVER () FROM t1
