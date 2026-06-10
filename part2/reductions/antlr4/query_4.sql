e1c8ff9a10f9ad4d55e3d487d471980486e34fbe
eamihai <eduardmihai958@gmail.com>
2026-06-10 21:04:21 +0200

    Add new reduced queries

--- part2/reductions/antlr4/query_4.sql
+++ part2/reductions/antlr4/query_4.sql
@@ -1,3 +1,4 @@
 CREATE TABLE t1 (  col0  ,  col1  ,  col2  ,  col3  ,  col4  );
-   INSERT INTO t1 (col0,col1,col2,col3,col4) VALUES (NULL,NULL,'av',-1252356455,'Yjca');  INSERT INTO t1 (col0,col1,col2,col4) VALUES (NULL,2049300292,'sYCWGtg8J','RxYwTBHZ3');  
+INSERT INTO t1 (col0,col1,col2,col3,col4) VALUES (NULL,NULL,'av',-1252356455,'Yjca');
+INSERT INTO t1 (col0,col1,col2,col4) VALUES (NULL,2049300292,'sYCWGtg8J','RxYwTBHZ3');
 SELECT LAG(- (- (-403961669))) OVER () AS win0, AVG( ((t1.col2 || 'HAFG'))) OVER (PARTITION BY col4 ) AS win1, ROW_NUMBER() OVER () AS win2 FROM t1;
